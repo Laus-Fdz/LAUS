156f6e9037b8bedb72084f9a2a6303cd5f551e74
Laus-Fdz <luryfernandez98@alumnos.cei.es>
2024-12-13 17:44:57 +0100

    cambios

---
+++ js/javascript.js
@@ -0,0 +1,15 @@
+// ----------------------------------------------------
+//                  MENÚ MÓVIL 
+// ----------------------------------------------------
+
+// creamos la constante del botón del menu y del contenedor del menú
+const menuButton = document.querySelector(".Nav-menu");
+const navLinks = document.querySelector(".Nav-menuLinks");
+
+// añadimos el eventListener de click al botón del menu
+menuButton.addEventListener("click", function () {
+    // alternamos la clase active en los enlaces de navegación
+    navLinks.classList.toggle("active");
+});
+
+
