7855f545fdf5d601cf159f5a40417fbf77a013d0
Laus-Fdz <luryfernandez98@alumnos.cei.es>
2025-01-04 19:14:46 +0100

    cambios

--- js/cart.js
+++ js/cart.js
@@ -74,6 +74,14 @@ function loadCart() {
                 <h3>${product.name}</h3>
                 <p>${product.price}€</p>
                 <p>Cantidad: ${product.quantity}</p>
+
+                <br>
+                <div class="Cart-item-quantity">
+                    <button class="Cart-quantity fas fa-minus" data-id="${product.id}"></button>
+                    <button class="Cart-quantity fas fa-plus" data-id="${product.id}"></button>
+                </div>
+
+                
                 <button class="Cart-remove fas fa-trash" data-id="${product.id}"></button>
             </div>
         `; // Agregar imagen, nombre, precio, cantidad y botón de eliminar
@@ -84,6 +92,16 @@ function loadCart() {
 
     cartTotalPrice.textContent = `${total.toFixed(2)}€`; // Mostrar el precio total
 
+    // Agregar evento de aumentar y disminuir cantidad de productos
+    document.querySelectorAll(".Cart-item-quantity").forEach(button => {
+        button.addEventListener("click", (event) => {
+            const productID = parseInt(event.target.getAttribute("data-id"));
+            const action = event.target.classList.contains("fa-plus") ? "increase" : "decrease";
+            updateQuantity(productID, action);
+        });
+    });
+
+
     // Agregar evento de eliminar producto
     document.querySelectorAll(".Cart-remove").forEach(button => {
         button.addEventListener("click", (event) => {
@@ -92,6 +110,29 @@ function loadCart() {
         });
     });
 }
+
+// Actualizar cantidad de productos
+function updateQuantity(productID, action) {
+    let cart = JSON.parse(localStorage.getItem("cart")) || []; // Obtener productos del carrito
+    const product = cart.find(item => item.id === productID); // Buscar el producto
+    const productIndex = cart.findIndex(item => item.id === productID); // Buscar el índice del producto
+
+    if (action === "increase") { // Si se aumenta la cantidad
+        product.quantity += 1; // Aumentar la cantidad
+    } else if (action === "decrease") { // Si se disminuye la cantidad
+        if (product.quantity > 1) { // Si la cantidad es mayor a 1
+            product.quantity -= 1; // Disminuir la cantidad
+        } else {
+            cart = cart.filter(item => item.id !== productID); // Filtrar el producto a eliminar
+        }
+    }
+
+    cart[productIndex] = product; // Actualizar el producto en el carrito
+    localStorage.setItem("cart", JSON.stringify(cart)); // Guardar el carrito en localStorage
+    loadCart(); // Recargar el carrito
+}
+
+
 
 // Eliminar producto del carrito
 function removeFromCart(productID) {
